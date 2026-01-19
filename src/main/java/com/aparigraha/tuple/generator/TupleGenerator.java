831fd20de0cd9cc055f3c6475c6fa172116bb3b0
AshwinSanthanam <ashwinsanthanam098@gmail.com>
2026-01-19 16:46:16 +0530

    Refactor - Move TupleGenerator's resuable templating code to JavaTemplate class

--- src/main/java/com/aparigraha/tuple/generator/TupleGenerator.java
+++ src/main/java/com/aparigraha/tuple/generator/TupleGenerator.java
@@ -9,17 +9,12 @@ import java.io.StringWriter;
 import java.io.Writer;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
+
+import static com.aparigraha.tuple.templates.JavaTemplate.*;
+
 
 public class TupleGenerator {
-    private static final String genericsPrefix = "T";
-    private static final String wildcard = "?";
-    private static final String tupleEqualsParameter = "that";
-    private static final String listToTupleParameter = "zipped";
-
     private static final PebbleTemplate template = getTemplate();
-
 
     public TupleSchema generate(TupleGenerationParams params) throws IOException {
         Writer writer = new StringWriter();
@@ -50,53 +45,6 @@ public class TupleGenerator {
                         "listToTupleSequence", listToTupleSequence(size)
                 )
         );
-    }
-
-    private static String genericsSequence(int size) {
-        return csvOf(generics(size));
-    }
-
-    private static String fields(List<String> fields) {
-        var generics = generics(fields.size()).toList();
-        return csvOf(range(fields.size()).map(index -> "%s %s".formatted(generics.get(index), fields.get(index))));
-    }
-
-    private static Stream<String> generics(int size) {
-        return range(size).map(index -> genericsPrefix + index);
-    }
-
-    private static String wildcardGenericSequence(int size) {
-        return csvOf(Stream.generate(() -> wildcard).limit(size));
-    }
-
-    private static String tupleEqualsCondition(List<String> fields) {
-        return logicalAndOf(
-                fields.stream().map(field -> "this.%s == %s.%s".formatted(field, tupleEqualsParameter, field))
-        );
-    }
-
-    private static String zipParameters(int size) {
-        return csvOf(range(size).map(index -> "Stream<T%d> stream%d".formatted(index, index)));
-    }
-
-    private static String objectStreamSequence(int size) {
-        return csvOf(range(size).map("(Stream<Object>) stream%d"::formatted));
-    }
-
-    private static String listToTupleSequence(int size) {
-        return csvOf(range(size).map(index -> "(%s%d) %s.get(%d)".formatted(genericsPrefix, index, listToTupleParameter, index)));
-    }
-
-    private static Stream<Integer> range(int size) {
-        return IntStream.range(0, size).boxed();
-    }
-
-    private static String csvOf(Stream<String> items) {
-        return items.reduce("%s, %s"::formatted).orElseThrow();
-    }
-
-    private static String logicalAndOf(Stream<String> conditions) {
-        return conditions.reduce("%s && %s"::formatted).orElseThrow();
     }
 
     private static PebbleTemplate getTemplate() {
---
+++ src/main/java/com/aparigraha/tuple/templates/JavaTemplate.java
@@ -0,0 +1,59 @@
+package com.aparigraha.tuple.templates;
+
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class JavaTemplate {
+    public static final String genericsPrefix = "T";
+    public static final String wildcard = "?";
+    public static final String tupleEqualsParameter = "that";
+    public static final String listToTupleParameter = "zipped";
+
+    public static String genericsSequence(int size) {
+        return csvOf(generics(size));
+    }
+
+    public static String fields(List<String> fields) {
+        var generics = generics(fields.size()).toList();
+        return csvOf(range(fields.size()).map(index -> "%s %s".formatted(generics.get(index), fields.get(index))));
+    }
+
+    public static Stream<String> generics(int size) {
+        return range(size).map(index -> genericsPrefix + index);
+    }
+
+    public static String wildcardGenericSequence(int size) {
+        return csvOf(Stream.generate(() -> wildcard).limit(size));
+    }
+
+    public static String tupleEqualsCondition(List<String> fields) {
+        return logicalAndOf(
+                fields.stream().map(field -> "this.%s == %s.%s".formatted(field, tupleEqualsParameter, field))
+        );
+    }
+
+    public static String zipParameters(int size) {
+        return csvOf(range(size).map(index -> "Stream<T%d> stream%d".formatted(index, index)));
+    }
+
+    public static String objectStreamSequence(int size) {
+        return csvOf(range(size).map("(Stream<Object>) stream%d"::formatted));
+    }
+
+    public static String listToTupleSequence(int size) {
+        return csvOf(range(size).map(index -> "(%s%d) %s.get(%d)".formatted(genericsPrefix, index, listToTupleParameter, index)));
+    }
+
+    public static Stream<Integer> range(int size) {
+        return IntStream.range(0, size).boxed();
+    }
+
+    public static String csvOf(Stream<String> items) {
+        return items.reduce("%s, %s"::formatted).orElseThrow();
+    }
+
+    public static String logicalAndOf(Stream<String> conditions) {
+        return conditions.reduce("%s && %s"::formatted).orElseThrow();
+    }
+}
